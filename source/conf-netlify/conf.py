2b97747166e1942e7210d719f44a738a28d22ff5
Anastasia Alexadrova <anastasia.alexandrova@percona.com>
2021-05-25 09:49:01 +0300

    DISTPG-237 Fix TOC display for percona web site
    Updated default conf.py and netlify conf.py so that theme specific sidebards are read from the corresponding file.
    modified:   source/conf-netlify/conf.py
    	modified:   source/conf.py

--- source/conf-netlify/conf.py
+++ source/conf-netlify/conf.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath("../"))
 from conf import *
 extensions.append('sphinx_gitstamp')
 extensions.append('sphinx_copybutton')
+html_sidebars['**']=['globaltoc.html', 'searchbox.html', 'localtoc.html', 'logo-text.html']
 html_theme = 'sphinx_material'
 html_theme_options = {
     'base_url': 'http://bashtage.github.io/sphinx-material/',
